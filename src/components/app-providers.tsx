cd14cd806fbea9b0437e09869a2bf0e7035862d8
Jan Reimann <jaanes79@gmail.com>
2026-04-01 08:40:39 +0300

    Fix invoice payment build import

--- src/components/app-providers.tsx
+++ src/components/app-providers.tsx
@@ -35,6 +35,7 @@ import {
   buildPackageAllocation,
   getClientAssessments,
   getClientNutritionPlans,
+  getInvoiceOutstandingAmount,
   getClientSessions,
   getInvoicePaidAmount,
   getSessionUnitPrice,
